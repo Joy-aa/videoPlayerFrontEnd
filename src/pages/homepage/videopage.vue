e7065861f1181ef58d6259cc6b168a0af2af78d7
Joy-aa <1663607838@qq.com>
2023-11-05 02:12:41 +0800

    del

--- src/pages/homepage/videopage.vue
+++ src/pages/homepage/videopage.vue
@@ -1187,50 +1187,6 @@ function forwardvideo() {
   alert("转发链接已复制到剪切板" + window.location.href)
 }
 
-function AutoSize (el) {
-  if (!(this instanceof AutoSize)) {
-    return new AutoSize(el)
-  }
-  if (!el) {
-    throw new Error('element can not be empty')
-  }
-  if (typeof el === 'string') {
-    el = document.querySelector(el)
-  }
-  this.el = el
-  const attrs = ['box-sizing', 'padding-top', 'padding-bottom', 'border-top', 'border-bottom']
-
-  // 初始化信息
-  this.heightOffset = 0
-  const style = window.getComputedStyle(el)
-  const [boxSizing, paddingTop, paddingBottom, borderTop, borderBottom] = attrs.map(item => style.getPropertyValue(item))
-  if (boxSizing === 'content-box') {
-    this.heightOffset = -(parseFloat(paddingTop)) - parseFloat(paddingBottom)
-  } else {
-    this.heightOffset = parseFloat(borderTop)  + parseFloat(borderBottom)
-  }
-  this.initEvent()
-}
-
-AutoSize.prototype = {
-  initEvent () {
-    this.listener = this.handleAction.bind(this)
-    this.el.addEventListener('input', this.listener, false)
-  },
-  destroy () {
-    this.el.removeEventListener('input', this.listener, false)
-    this.listener = null
-  },
-  handleAction (e) {
-    const event = e || window.event
-    const target = event.target || event.srcElement
-    target.style.height = ''
-    target.style.height = target.scrollHeight + this.heightOffset + 'px'
-  }
-}
-// AutoSize("div")
-// AutoSize("input")
-
 const comments = [
   {
     name: "用户1", head: require("../../assets/img.png"),
